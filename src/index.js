e56c1b6d3dc62329d8bf353e906c0a4741e90c6b
Hamilton Smith <smithhamilton4@gmail.com>
2017-01-22 15:59:57 -0500

    improved search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import VideoDetail from './components/video_detail.js';
 import VideoList from './components/video_list';
@@ -19,20 +20,28 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		this.videoSearch('surfboards');	
+		
+
+}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({ 
 				videos: videos,
 				selectedVideo: videos[0]
 			});
 			// this.setState({ videos: videos });
 		});
+	};
 
-
-	}
+	
 	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+
 		return (
 		  <div>
-			<SearchBar />
+			<SearchBar onSearchTermChange={videoSearch} />
 			<VideoDetail video={this.state.selectedVideo} />
 			<VideoList  
 				onVideoSelect={selectedVideo => this.setState({selectedVideo})}
